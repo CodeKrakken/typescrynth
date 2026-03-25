bd1fabf2afc8eb616886cdb4b58ac133ce50b010
CodeKrakken <newholm1@hotmail.com>
2026-03-25 17:45:04 +0000

    Remove useCallback import from keyboard

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { synth } from '../synth/Synth';
 import './keyboard.css'
 import { keys } from '../data'
