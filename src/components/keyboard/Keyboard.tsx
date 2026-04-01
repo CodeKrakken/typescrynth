e11cf871ac8e9c63da90b35f712bc727519d8499
CodeKrakken <newholm1@hotmail.com>
2026-04-01 18:23:27 +0100

    Attempt to prevent scrolling on mobile

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -107,19 +107,27 @@ export default function Keyboard() {
       endHold(key)
     }
 
+    const preventZoom = (e: TouchEvent) => {
+    if (e.touches.length > 1) {
+      e.preventDefault()
+    }
+  }
+
 
     // attach event listeners
 
     document.addEventListener     ('keydown'    , handleKeyDown     as EventListener);  
     document.addEventListener     ('keyup'      , handleKeyUp       as EventListener);  
-    document.addEventListener     ('touchstart' , handleTouchStart  as EventListener);  
-    document.addEventListener     ('touchend'   , handleTouchEnd    as EventListener);  
-      
+    document.addEventListener     ('touchstart' , handleTouchStart  as EventListener, { passive: false });  
+    document.addEventListener     ('touchend'   , handleTouchEnd    as EventListener, { passive: false });  
+    document.addEventListener     ('touchmove'  , preventZoom       as EventListener, { passive: false })  
+
     return () => {  
       document.removeEventListener('keydown'    , handleKeyDown     as EventListener);  
       document.removeEventListener('keyup'      , handleKeyUp       as EventListener);  
       document.removeEventListener('touchstart' , handleTouchStart  as EventListener);  
-      document.removeEventListener('touchend'   , handleTouchEnd    as EventListener);  
+      document.removeEventListener('touchend'   , handleTouchEnd    as EventListener);
+      document.removeEventListener('touchmove'  , preventZoom       as EventListener);
     };  
   }, []);
 
