22581b76b8d98136a6ae885c08fdeb6db75d6e67
CodeKrakken <newholm1@hotmail.com>
2026-04-09 12:41:57 +0100

    Combine press and release into one function

--- src/components/keyboard/Keyboard.test.tsx
+++ src/components/keyboard/Keyboard.test.tsx
@@ -37,13 +37,14 @@ jest.mock('./functions', () => ({
   generateKeyColours: jest.fn()
 }))
 
-const press = (key: string, repeat: string = '') => {
-  if (repeat) return fireEvent.keyDown(document, { key: key, repeat: true })
-  return fireEvent.keyDown(document, { key: key })
-}
 
-const release = (key: string) => {
-  return fireEvent.keyUp(document, { key: key })
+// test helper functions
+
+const pressAndRelease = (key: string, repeat: string = '') => {
+  if (repeat) return fireEvent.keyDown(document, { key: key, repeat: true })
+
+  fireEvent.keyDown(document, { key: key })
+  fireEvent.keyUp(document, { key: key })
 }
 
 const touchAndRelease = (key: string) => {
@@ -61,62 +62,38 @@ describe('Keyboard', () => {
 
   beforeEach(() => {
     jest.clearAllMocks()
+    render(<Keyboard />)
   })
+  
 
-  it('renders all keys', () => {
-    const { getByText } = render(<Keyboard />)
-
-    expect(getByText('z')).toBeInTheDocument()
-    expect(getByText('q')).toBeInTheDocument()
-  })
+  
 
   it('handles keydown and triggers synth', () => {
-    render(<Keyboard />)
-    
-    press('z')
+    pressAndRelease('z')
 
     expect(synth.resume).toHaveBeenCalled()
     expect(synth.toggleAttribute).toHaveBeenCalledWith('baseFreq', '16.35')
   })
 
   it('does not trigger on repeated keydown', () => {
-    render(<Keyboard />)
-
-    press('z', 'repeat')
+    pressAndRelease('z', 'repeat')
 
     expect(synth.toggleAttribute).not.toHaveBeenCalled()
   })
 
   it('handles keyup and releases note', () => {
-    render(<Keyboard />)
-
-    press('z')
-    release('z')
+    pressAndRelease('z')
 
     expect(synth.toggleAttribute).toHaveBeenCalledTimes(2)
   })
 
   it('does not toggle attribute for waveform on keyup', () => {
-    render(<Keyboard />)
-
-    press('q')
-    release('q')
+    pressAndRelease('q')
 
     expect(synth.toggleAttribute).toHaveBeenCalledTimes(1)
   })
 
-  it('handles touchstart', () => {
-    const { container } = render(<Keyboard />)
-    const target = container.querySelector('[data-key="z"]')!
-
-    fireEvent.touchStart(target, { target: target })
-
-    expect(synth.toggleAttribute).toHaveBeenCalledWith('baseFreq', '16.35')
-  })
-
   it('ignores touchstart when no data-key is present', () => {
-    render(<Keyboard />)
-
     const div = document.createElement('div')
     const event = new Event('touchstart', { bubbles: true })
 
@@ -129,8 +106,6 @@ describe('Keyboard', () => {
   })
   
   it('ignores touchend when no data-key is present', () => {
-    render(<Keyboard />)
-
     const div = document.createElement('div')
     const event = new Event('touchend', { bubbles: true })
 
@@ -142,17 +117,10 @@ describe('Keyboard', () => {
     expect(synth.toggleAttribute).not.toHaveBeenCalled()
   })
 
-  it('handles touchend', () => {
-
-    touchAndRelease('z')
-
-    expect(synth.toggleAttribute).toHaveBeenCalledTimes(2)
-  })
+  
 
   it('ignores unknown keys', () => {
-    render(<Keyboard />)
-
-    press('a')
+    pressAndRelease('a')
 
     expect(synth.toggleAttribute).not.toHaveBeenCalled()
   })
@@ -164,39 +132,31 @@ describe('Keyboard', () => {
   })
 
   it('runs isActive and receives true', () => {
-    render(<Keyboard />)
-
     synth.settings.attributes.waveforms = ['sine']
 
-    press('q')
+    pressAndRelease('q')
 
     expect(isActive('q')).toBeTruthy()
   })
 
   it('handles inactive non-note keys', () => {
-    render(<Keyboard />)
-
     synth.settings.attributes.waveforms = []
 
-    press('q')
+    pressAndRelease('q')
 
     expect(synth.toggleAttribute).toHaveBeenCalled()
   })
 
 
   it('applies colour when key is active', () => {
-    render(<Keyboard />)
-
     synth.settings.attributes.waveforms = ['sine']
 
-    press('q')
+    pressAndRelease('q')
 
     expect(randomColour).toHaveBeenCalled()
   })
 
   it('prevents zoom when multiple touches', () => {
-    render(<Keyboard />)
-
     const event = new Event('touchmove', { bubbles: true })
     const preventDefault = jest.fn()
 
@@ -209,8 +169,6 @@ describe('Keyboard', () => {
   })
 
   it('does not prevent zoom for single touch', () => {
-    render(<Keyboard />)
-
     const event = new Event('touchmove')
 
     Object.defineProperty(event, 'touches', { value: [{}] })
@@ -219,5 +177,35 @@ describe('Keyboard', () => {
     document.dispatchEvent(event)
 
     expect(event.preventDefault).not.toHaveBeenCalled()
+  })
+  
+})
+
+describe('Keyboard', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('renders all keys', () => {
+    const { getByText } = render(<Keyboard />)
+
+    expect(getByText('z')).toBeInTheDocument()
+    expect(getByText('q')).toBeInTheDocument()
+  })
+
+  it('handles touchstart', () => {
+    const { container } = render(<Keyboard />)
+    const target = container.querySelector('[data-key="z"]')!
+
+    fireEvent.touchStart(target, { target: target })
+
+    expect(synth.toggleAttribute).toHaveBeenCalledWith('baseFreq', '16.35')
+  })
+
+  it('handles touchend', () => {
+
+    touchAndRelease('z')
+
+    expect(synth.toggleAttribute).toHaveBeenCalledTimes(2)
   })
 })
